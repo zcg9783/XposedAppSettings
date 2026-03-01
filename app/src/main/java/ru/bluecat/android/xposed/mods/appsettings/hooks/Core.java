f156a4be6b8f85c4c866c2772601e76924500c26
zcg9783 <zcg5665@126.com>
2026-03-01 11:10:17 +0800

    更新 Core.java

--- app/src/main/java/ru/bluecat/android/xposed/mods/appsettings/hooks/Core.java
+++ app/src/main/java/ru/bluecat/android/xposed/mods/appsettings/hooks/Core.java
@@ -1,7 +1,9 @@
 package ru.bluecat.android.xposed.mods.appsettings.hooks;
 
-
 import android.content.res.XModuleResources;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -13,57 +15,75 @@ import ru.bluecat.android.xposed.mods.appsettings.Constants;
 
 public class Core implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
-	private static XSharedPreferences prefs;
-	public static XModuleResources mResources;
+    private static XSharedPreferences prefs;
+    public static XModuleResources mResources;
 
-	@Override
-	public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) {
+    private static final Map<String, Integer> PRESET_DPI_MAP = new HashMap<>();
+    static {
+        PRESET_DPI_MAP.put("bin.mt.plus", 200);
+        PRESET_DPI_MAP.put("com.xtc.b", 200);
+    }
 
-		mResources = XModuleResources.createInstance(startupParam.modulePath, null);
-		if(prefs == null) prefs = getModulePrefs();
+    public static int getEffectiveDpi(String packageName, XSharedPreferences prefs) {
+        int dpi = prefs.getInt(packageName + Constants.PREF_DPI, 0);
+        if (dpi != 0) {
+            return dpi;
+        }
+        Integer preset = PRESET_DPI_MAP.get(packageName);
+        if (preset != null) {
+            return preset;
+        }
+        return prefs.getInt(Constants.PREF_DEFAULT + Constants.PREF_DPI, 0);
+    }
 
-		if(prefs != null) {
-			prefs.reload();
-			onZygoteLoad.adjustSystemDimensions(prefs);
-			onZygoteLoad.dpiInSystem(prefs);
-			onZygoteLoad.activitySettings(prefs);
-		}
-	}
+    @Override
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) {
 
-	public static XSharedPreferences getModulePrefs() {
-		XSharedPreferences pref = new XSharedPreferences(Constants.MY_PACKAGE_NAME, Constants.PREFS);
-		return pref.getFile().canRead() ? pref : null;
-	}
+        mResources = XModuleResources.createInstance(startupParam.modulePath, null);
+        if(prefs == null) prefs = getModulePrefs();
 
-	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) {
-		if (lpparam.packageName.equals(Constants.MY_PACKAGE_NAME)) {
-			XposedHelpers.findAndHookMethod(Constants.MY_PACKAGE_NAME + ".MainActivity",
-					lpparam.classLoader,
-					"isModuleActive",
-					XC_MethodReplacement.returnConstant(true));
-		}
+        if(prefs != null) {
+            prefs.reload();
+            onZygoteLoad.adjustSystemDimensions(prefs);
+            onZygoteLoad.dpiInSystem(prefs);
+            onZygoteLoad.activitySettings(prefs);
+        }
+    }
 
-		if(prefs != null) prefs.reload(); else return;
+    public static XSharedPreferences getModulePrefs() {
+        XSharedPreferences pref = new XSharedPreferences(Constants.MY_PACKAGE_NAME, Constants.PREFS);
+        return pref.getFile().canRead() ? pref : null;
+    }
 
-		if (lpparam.packageName.equals("android")) {
-			AndroidServer.resident(lpparam, prefs);
-			AndroidServer.recentTasks(lpparam, prefs);
-			AndroidServer.UnrestrictedGetTasks(lpparam, prefs);
-			AndroidServer.notificationManager(lpparam, prefs);
-			PackagePermissions.init(lpparam, prefs);
-		}
-		onPackageLoad.screenSettings(lpparam, prefs);
-		onPackageLoad.soundPool(lpparam, prefs);
-		onPackageLoad.legacyMenu(lpparam, prefs);
-	}
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) {
+        if (lpparam.packageName.equals(Constants.MY_PACKAGE_NAME)) {
+            XposedHelpers.findAndHookMethod(Constants.MY_PACKAGE_NAME + ".MainActivity",
+                    lpparam.classLoader,
+                    "isModuleActive",
+                    XC_MethodReplacement.returnConstant(true));
+        }
 
-	static boolean isActive(XSharedPreferences prefs, String packageName) {
-		return prefs.getBoolean(packageName + Constants.PREF_ACTIVE, false);
-	}
+        if(prefs != null) prefs.reload(); else return;
 
-	static boolean isActive(XSharedPreferences prefs, String packageName, String sub) {
-		return prefs.getBoolean(packageName + Constants.PREF_ACTIVE, false) &&
-				prefs.getBoolean(packageName + sub, false);
-	}
+        if (lpparam.packageName.equals("android")) {
+            AndroidServer.resident(lpparam, prefs);
+            AndroidServer.recentTasks(lpparam, prefs);
+            AndroidServer.UnrestrictedGetTasks(lpparam, prefs);
+            AndroidServer.notificationManager(lpparam, prefs);
+            PackagePermissions.init(lpparam, prefs);
+        }
+        onPackageLoad.screenSettings(lpparam, prefs);
+        onPackageLoad.soundPool(lpparam, prefs);
+        onPackageLoad.legacyMenu(lpparam, prefs);
+    }
+
+    static boolean isActive(XSharedPreferences prefs, String packageName) {
+        return prefs.getBoolean(packageName + Constants.PREF_ACTIVE, false);
+    }
+
+    static boolean isActive(XSharedPreferences prefs, String packageName, String sub) {
+        return prefs.getBoolean(packageName + Constants.PREF_ACTIVE, false) &&
+                prefs.getBoolean(packageName + sub, false);
+    }
 }
